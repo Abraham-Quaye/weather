c496bb4a62d61ee8c6d3f6943f66022c45403e69
Abraham Quaye <quayeabraham29@gmail.com>
2024-03-17 20:36:11 -0600

    improved script from automatic running of workflow

--- code/zsh/workflow_scheduler.sh
+++ code/zsh/workflow_scheduler.sh
@@ -2,16 +2,19 @@
 
 cd ~/weather && echo "Now in Weather directory" || "Could not find Weather Directory"
 
-# # activate conda environment
-# eval "$(conda shell.zsh hook)"
-# conda activate weather &&
-# echo "Weather environment activated successfully!!! " ||
-# (echo "Environment activation Error!!!" ; exit)
+# activate conda environment
+eval "$(conda shell.zsh hook)"
+conda activate weather &&
+echo "Weather environment activated successfully!!! " ||
+(echo "Environment activation Error!!!" ; exit)
 
-# snakemake -c1 -R fetch_ghcnd_data
-# snakemake -c1 -R run_project
+snakemake -c1 -R fetch_ghcnd_data
+snakemake -c1 -R run_project
 
-git add .
-echo "files staged to commit"
-git commit -m "Added script to run pipeline on schedule"
+run_date=$(date | date | cut -d " " -f 2,3)
+
+git add plots/prcp_plot.png
+
+git commit -m "Updated Plot for $run_date"
+
 git push
