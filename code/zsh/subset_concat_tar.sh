9b8cb3d35fb44d0b9cbe0686b3e7b35ef0bf8ac9
Abraham Quaye <quayeabraham29@gmail.com>
2024-11-17 12:02:54 -0700

    fixed error with grep flag

--- code/zsh/subset_concat_tar.sh
+++ code/zsh/subset_concat_tar.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data/processed/temp
 
-tar Oxvzf data/ghcnd_data/ghcnd_all.tar.gz | grep -P "(PRCP|TMAX)" | \
+tar Oxvzf data/ghcnd_data/ghcnd_all.tar.gz | grep -E "(PRCP|TMAX)" | \
 split -l 250000 --filter 'gzip > data/processed/temp/$FILE.gz'
